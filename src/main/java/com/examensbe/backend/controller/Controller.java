448dae84fa86052f9a71eeefc480a7e683185152
Mickemannen86 <112933571+Mickemannen86@users.noreply.github.com>
2023-12-28 12:51:08 +0100

    publish backend

---
+++ src/main/java/com/examensbe/backend/controller/Controller.java
@@ -0,0 +1,23 @@
+package com.examensbe.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/* Skapa API */
+
+@RestController
+@RequestMapping("/api/v1/test1")
+public class Controller {
+
+    @GetMapping()
+    public ResponseEntity<String> helloMikey() {
+        return ResponseEntity.ok("Hello Mikey, API connection succed");
+
+
+    }
+
+
+
+}
---
+++ src/main/java/com/examensbe/backend/repository/IRepository.java
@@ -0,0 +1,17 @@
+package com.examensbe.backend.repository;
+
+/*
+import com.examensbe.backend.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+
+ */
+public interface IRepository {
+
+
+}
